d63f7797756ddf7714bc2d09c63f2c0897bb0320
Patnala, Saran Teja <VNVSaranTeja.Patnala+pega@in.pega.com>
2026-04-22 20:38:38 +0530

    Added initial gin handlers for health and info

--- main/main.go
+++ main/main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Welcome to the Event Booking System!")
-	gin.New().Run(":8080")
+
+	server := gin.Default()
+
+	server.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "UP",
+		})
+	})
+
+	server.GET("/info", getInfo)
+
+	if err := server.Run("localhost:8080"); err != nil {
+		fmt.Printf("Failed to start server: %s\n", err.Error())
+	}
+}
+
+func getInfo(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"service": "Event Booking System",
+		"version": "1.0.0",
+	})
 }
